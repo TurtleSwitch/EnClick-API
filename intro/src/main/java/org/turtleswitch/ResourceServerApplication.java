c185af7bb3d2c9555d90cffced25816b857e11d3
LeeJungMok <mearyne@gmail.com>
2023-11-30 10:30:39 +0900

    unused import 삭제

--- intro/src/main/java/org/turtleswitch/ResourceServerApplication.java
+++ intro/src/main/java/org/turtleswitch/ResourceServerApplication.java
@@ -2,7 +2,6 @@ package org.turtleswitch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
--- order/src/main/java/org/turtleswitch/Controller/OrderController.java
+++ order/src/main/java/org/turtleswitch/Controller/OrderController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.turtleswitch.Service.OrderService;
-import org.turtleswitch.model.OrderGroup;
 
 @RestController
 @RequestMapping("/order")
