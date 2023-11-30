50fa6d6a1e148f62b50016bd1dcf623cf1082be7
hmkim <elppapie@naver.com>
2023-11-30 11:12:00 +0900

    :tada: user 모듈 model 추가
    Address, CartMst, CartDtl, CartDtlPK, Favor, Seller, User

---
+++ user/src/main/java/com/turtleswitch/model/CartDtlPK.java
@@ -0,0 +1,21 @@
+package com.turtleswitch.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class CartDtlPK {
+
+    @Column(name="ITEM_UUID")
+    private String itemUUID;
+
+    @Column(name="OPTION_ID")
+    private String optionId;
+}
