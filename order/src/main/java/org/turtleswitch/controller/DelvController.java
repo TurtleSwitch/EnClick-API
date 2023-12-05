e1b338860246f4e96c6566d5c15ee2f31ee7d7ef
LeeJungMok <mearyne@gmail.com>
2023-12-05 11:44:52 +0900

    :tada: 주문 모듈 Controller 추가

--- order/src/main/java/org/turtleswitch/controller/DelvController.java
+++ order/src/main/java/org/turtleswitch/controller/DelvController.java
@@ -1,17 +1,38 @@
 package org.turtleswitch.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.turtleswitch.dto.ResponseDto;
 
 @RestController
 @RequestMapping("/delv")
 public class DelvController {
 
-    @GetMapping("/")
-    public String test(@RequestBody String str) {
-        return str;
+    /**
+     * 배송 정보 등록
+     */
+    @PostMapping("/save/{orderMstUuid}")
+    public ResponseEntity<?> save(@PathVariable @Valid String orderMstUuid, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "배송 등록 성공", null), HttpStatus.CREATED);
+    }
+
+    /**
+     * 배송MST 정보 조회
+     */
+    @GetMapping("/{orderMstUuid}")
+    public ResponseEntity<?> table(@PathVariable @Valid String orderMstUuid, @RequestBody @Valid Object param, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "배송 정보 조회 성공", null), HttpStatus.OK);
+    }
+
+    /**
+     * 배송MST 상태 변경 성공
+     */
+    @PutMapping("/{stat}/{orderMstUuid}")
+    public ResponseEntity<?> stat(@PathVariable @Valid String stat, @PathVariable @Valid String orderMstUuid, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "배송 상태 변경 성공", null), HttpStatus.OK);
     }
 
 }
--- order/src/main/java/org/turtleswitch/controller/OrderController.java
+++ order/src/main/java/org/turtleswitch/controller/OrderController.java
@@ -18,8 +18,36 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    /**
+     * 주문 등록시 사용되는 컨트롤러
+     * 회원 비회원 모두 동일한 메소드를 사용한다
+     */
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody @Valid OrderGroupReqDto requestDto, BindingResult bindingResult) {
-        return new ResponseEntity<>(new ResponseDto<>(1, "주문 저장 성공", orderService.save(requestDto)), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseDto<>(1, "주문 성공", null), HttpStatus.CREATED);
+    }
+
+    /**
+     * 유저 아이디와 param을 받아서 적절한 주문 리스트를 조회한다
+     */
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> orderList(@PathVariable @Valid String userId, @RequestBody @Valid Object param, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "유저 주문 리스트 획득 성공", null), HttpStatus.OK);
+    }
+
+    /**
+     * orderMst 상태 변경
+     */
+    @PutMapping("/group/{stat}/{orderUuid}")
+    public ResponseEntity<?> changeStat(@PathVariable @Valid String orderUuid, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "주문 상태 변경 성공", null), HttpStatus.OK);
+    }
+
+    /**
+     * orderMstUuid를 통해 주문 상세정보를 읽는다
+     */
+    @GetMapping("/master/{orderMstUuid}")
+    public ResponseEntity<?> getOrderDetail(@PathVariable @Valid String orderMstUuid, BindingResult bindingResult) {
+        return new ResponseEntity<>(new ResponseDto<>(1, "주문 정보 조회 성공", null), HttpStatus.OK);
     }
 }
